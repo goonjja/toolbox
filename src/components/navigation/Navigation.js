092427b6e7f6bc68223239cd36859347794deeff
Sergey Vedernikov <vedernikov.s@gmail.com>
2022-08-29 17:58:24 +0200

    Build fix

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -6,7 +6,7 @@ class Navigation extends React.Component {
     return (
       <ul className="flex sm:flex-col overflow-hidden content-center justify-between bg-slate-400">
         <li className="py-2 hover:bg-indigo-300 rounded">
-          <a className="truncate" href="#">
+          <a className="truncate" href="./">
             {/* <img
               src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg"
               className="w-7 sm:mx-2 mx-4 inline"
@@ -15,7 +15,7 @@ class Navigation extends React.Component {
           </a>
         </li>
         <li className="py-2 hover:bg-indigo-300 rounded">
-          <a className="truncate" href="#">
+          <a className="truncate" href="./">
             {/* <img
               src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg"
               className="w-7 sm:mx-2 mx-4 inline"

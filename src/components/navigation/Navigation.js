f423caf8d65c0de722b8278fa393a93f479a2135
Sergey Vedernikov <vedernikov.s@gmail.com>
2022-08-29 17:51:41 +0200

    Rewrite build

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -7,19 +7,19 @@ class Navigation extends React.Component {
       <ul className="flex sm:flex-col overflow-hidden content-center justify-between bg-slate-400">
         <li className="py-2 hover:bg-indigo-300 rounded">
           <a className="truncate" href="#">
-            <img
+            {/* <img
               src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg"
               className="w-7 sm:mx-2 mx-4 inline"
-            />
+            /> */}
             <span className="hidden sm:inline">Color Tools</span>
           </a>
         </li>
         <li className="py-2 hover:bg-indigo-300 rounded">
           <a className="truncate" href="#">
-            <img
+            {/* <img
               src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg"
               className="w-7 sm:mx-2 mx-4 inline"
-            />
+            /> */}
             <span className="hidden sm:inline">Music Tools</span>
           </a>
         </li>
